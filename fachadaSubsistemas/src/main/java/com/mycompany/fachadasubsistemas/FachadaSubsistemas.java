13e4d8ec2017b803a3d5fe95e839eb3e4b32ef2c
Juamp-i5 <juan.olivarria252886@potros.itson.edu.mx>
2025-03-19 22:08:14 -0700

    Creacion proyecto

---
+++ fachadaSubsistemas/src/main/java/com/mycompany/fachadasubsistemas/FachadaSubsistemas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fachadasubsistemas;
+
+/**
+ *
+ * @author Jp
+ */
+public class FachadaSubsistemas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ infraestructura/src/main/java/com/mycompany/infraestructura/Infraestructura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.infraestructura;
+
+/**
+ *
+ * @author Jp
+ */
+public class Infraestructura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
