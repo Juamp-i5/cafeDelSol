2c09bac82cb09f1aa69a95f0ff73e081042c1fdb
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-03-20 23:59:21 -0700

    .

--- presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
+++ presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -13,6 +13,7 @@ import control.ControlNavegacion;
 public class Presentacion {
 
     public static void main(String[] args) {
+        //.
         ControlNavegacion.mostrarPantallaMenuPrincipal();
     }
 }
