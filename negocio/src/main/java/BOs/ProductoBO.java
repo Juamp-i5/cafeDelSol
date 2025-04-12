41cc418bd2b93b50ce5bbb19d70f8903cdfed707
Rodrigo-Tovar <rodrigo.tovar253226@potros.itson.edu.mx>
2025-04-11 23:51:09 -0700

    proyecto de DTOs

---
+++ negocio/src/main/java/BOs/ProductoBO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BOs;
+
+import DAOs.ProductoDAOImp;
+import DTOs.ProductoMostrarDTO;
+import entidades.Producto;
+import exception.NegocioException;
+import exception.persistenciaException;
+import interfaces.IProducto;
+import interfacesBO.IProductoBO;
+import interfacesMapper.IProductoMapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mapper.ProductoMapper;
+
+/**
+ *
+ * @author rodri
+ */
+public class ProductoBO implements IProductoBO{
+
+    IProducto productoDAO = ProductoDAOImp.getInstance();
+    IProductoMapper productoMapper = ProductoMapper.getInstance();
+    
+    @Override
+    public List<ProductoMostrarDTO> cargarProductos() throws NegocioException {
+        try {
+            
+            List<Producto> productos = productoDAO.buscarTodos();
+            List<ProductoMostrarDTO> productosDTO = new ArrayList<>();
+            
+            for (Producto producto : productos) {
+                ProductoMostrarDTO productoDTO = productoMapper.aDTO(producto);
+                
+            }
+            
+            
+        } catch (persistenciaException ex) {
+            Logger.getLogger(ProductoBO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new NegocioException("Error al cargar los productos");
+        }
+        
+    }
+    
+}
