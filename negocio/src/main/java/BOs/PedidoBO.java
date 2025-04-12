851aa8b563b18252bbca12875d5e505169c4e892
normabeltraan <norma07beltran@hotmail.com>
2025-04-11 22:41:13 -0700

    patrón Observer listo (simulado ya que no hay ingredientes en este caso de uso)

--- negocio/src/main/java/BOs/PedidoBO.java
+++ negocio/src/main/java/BOs/PedidoBO.java
@@ -6,17 +6,31 @@ package BOs;
 
 import DTOs.PedidoDTO;
 import interfacesBO.IPedidoBO;
+import java.util.ArrayList;
+import java.util.List;
+import observers.interfaces.NuevaVentaObserver;
 
 /**
  *
  * @author rodri
  */
-public class PedidoBO implements IPedidoBO{
+public class PedidoBO implements IPedidoBO {
+
+    private List<NuevaVentaObserver> observers = new ArrayList<>();
+
+    public void agregarObserver(NuevaVentaObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notificarObservers() {
+        for (NuevaVentaObserver observer : observers) {
+            observer.update();
+        }
+    }
 
     @Override
     public PedidoDTO registrarPedido(PedidoDTO pedidoDTO) {
-        
+        notificarObservers();
     }
-    
-    
+
 }
---
+++ negocio/src/main/java/observers/BaristaObserver.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package observers;
+
+import DTOs.PedidoDTO;
+import java.util.ArrayList;
+import java.util.List;
+import observers.interfaces.NuevaVentaObserver;
+
+/**
+ *
+ * @author norma
+ */
+public class BaristaObserver implements NuevaVentaObserver {
+
+    private List<String> notificaciones = new ArrayList<>();
+    int cont = 0;
+
+    @Override
+    public void update() {
+        if (esStockBajo() == true) {
+            String mensaje = "Alerta: Stock bajo para de ingrediente BLAH";
+            notificaciones.add(mensaje);
+            enviarAlerta(mensaje);
+        }
+    }
+
+    private boolean esStockBajo() {
+        //Aqui seria la lógica para checar el bajo stock.
+        //Pero como no tenemos ingredientes en este caso de uso...
+        if(cont%2 == 0){
+            cont = cont + 1;
+            return true;
+        }
+        return false;
+    }
+
+    private void enviarAlerta(String mensaje) {
+        System.out.println("🔔 Barista: " + mensaje); 
+    }
+
+    public List<String> getNotificaciones() {
+        return notificaciones;
+    }
+}
